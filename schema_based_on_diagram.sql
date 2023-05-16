73adec4576b2019cb91be2b55ac7708c519b3335
karan jain <karanjain2212@gmail.com>
2023-05-16 20:50:23 +0530

    created tables in schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -25,3 +25,37 @@ CREATE TABLE treatments (
     name VARCHAR(20) NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE TABLE medical_treatment_history(
+  treatement_id INTEGER REFERENCES treatments(id),
+  medical_history_id INTEGER REFERENCES medical_histories(id),
+  PRIMARY KEY (treatement_id, medical_history_id) 
+);
+
+CREATE TABLE invoices (
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP,
+    medical_histories INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT "fk_medical_histories.id"
+    FOREIGN KEY (medical_histories)
+     REFERENCES medical_histories(id)
+        ON DELETE CASCADE
+);
+
+
+
+CREATE TABLE invoice_items(
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+   unit_price DECIMAL NOT NULL,
+   quantity INT NOT NULL,
+   total_price DECIMAL NOT NULL,
+   invoice_id INTEGER,
+   treatment_id INTEGER ,
+    PRIMARY KEY (id),
+    CONSTRAINT "fk_invoice.id"
+    FOREIGN KEY (invoice_id)
+     REFERENCES invoices(id)
+        ON DELETE CASCADE
+);
